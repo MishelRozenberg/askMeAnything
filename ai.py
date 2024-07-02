3c67d3e94efc7efee9a204c22f1c072ed3d6a17a
mishel <mishel>
2024-07-02 21:29:19 +0300

    git add openai questioning

---
+++ ai.py
@@ -0,0 +1,21 @@
+import openai
+from openai import AsyncOpenAI, OpenAIError
+import os
+
+# models
+GPT_MODEL = "gpt-3.5-turbo"
+
+client = AsyncOpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+
+async def ask_openai(question):
+    try:
+        response = await client.chat.completions.create(
+            model=GPT_MODEL,
+            messages=[
+                {"role": "user", "content": question}
+            ]
+        )
+        return response.choices[0].message.content
+    except OpenAIError as ai_error:
+        raise ai_error
